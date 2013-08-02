5cf21025ffb26e24c0c7b6979a5bddd40633dce4
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-08-02 17:21:36 +0200

    fixed stylesheet creation script

--- bin/rebuild-stylesheets.sh
+++ bin/rebuild-stylesheets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 listOfLESS=(
-WebContent/css/theme-dark
-WebContent/css/theme-light
+src/main/webapp/css/theme-dark
+src/main/webapp/css/theme-light
 );
 for i in ${listOfLESS[@]};do
     lessc ${i}.less > ${i}.css;
