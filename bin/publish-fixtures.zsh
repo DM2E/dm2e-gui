4561bd5af42f5f2ff5df1ec9d7bcd30e45367149
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-09-26 08:04:21 +0200

    Publish fixtures script publishes files as well as wf, wf config and wf job

--- bin/publish-fixtures.zsh
+++ bin/publish-fixtures.zsh
@@ -1,5 +1,7 @@
 #!env zsh
 
+echoerr() { echo "$@" 1>&2; }
+
 tempfile=$(mktemp)
 workflow_file=$(mktemp)
 
@@ -17,35 +19,45 @@ if [ -z "$SRV" ];then
 fi
 
 
+echoerr "# Replacing boilerplate URL with real URL"
 boilerplate_url="http://localhost:9998/api"
 if [ "$SRV" != "$boilerplate_url" ];then
     echo "Replacing... $boilerplate_url with $SRV"
     sed -i "s!http://localhost:9998/api!$SRV!" $workflow_file
 fi
 
+##
+## FIXME this will break if dm2e-ws repo is not in the parent directory
+## TODO metadata
+echoerr "# publish some files"
+FILE1=$(zsh bin/ingest-file.sh ../dm2e-ws/src/test/resources/mappings/xslt/KBA_BBAW_TO_EDM.xsl)
+echo "FILE1='$FILE1'"
+FILE2=$(zsh bin/ingest-file.sh ../dm2e-ws/src/test/resources/provider-examples/dta/grimm_meistergesang_1811.TEI-P5.xml)
+echo "FILE2='$FILE2'"
 
-# post workflow
+echoerr "# post workflow"
 WORKFLOW=$(POST -H $CT_JSON $SRV/workflow -d @$workflow_file \
     2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
+echo "WORKFLOW='$WORKFLOW'"
 
-# get empty config
+echoerr "# get empty config"
 GETJ $WORKFLOW/blankConfig 2>/dev/null | sed 's/bottles of beer on the wall/flailing piglets/' > $tempfile
 sed -i '/^{/a \
 "http://purl.org/dc/terms/creator":"http://omnom.hu-berlin.de/api/user/command-line",
 ' $tempfile
 
 
-# persist it
+echoerr "# persist it"
 CONFIG=$(POST -H $CT_JSON $SRV/config -d @$tempfile \
     2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
+echo "CONFIG='$CONFIG'"
 
-# run a job
+echoerr "# run a job"
 # echo POST -H "$CT_TEXT" "$WORKFLOW" -d "$CONFIG"
 # PUT "-H$CT_TEXT" "$WORKFLOW" -d "$CONFIG"
 JOB=$(PUT "-H$CT_TEXT" "$WORKFLOW" -d "$CONFIG" \
     2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
-
-echo "WORKFLOW='$WORKFLOW'"
-echo "CONFIG='$CONFIG'"
 echo "JOB='$JOB'"
+
+
 echo $tempfile
