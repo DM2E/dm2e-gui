36041dbab1b0dab95d715faffc3bb669f0e30c89
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-09-26 13:56:41 +0200

    Publish a XSLT-Publish workflow using publish-fixtures

--- bin/publish-fixtures.zsh
+++ bin/publish-fixtures.zsh
@@ -1,13 +1,6 @@
 #!env zsh
-
-echoerr() { echo "$@" 1>&2; }
-
-tempfile=$(mktemp)
-workflow_file=$(mktemp)
-
+#
 source bin/curl_rest.sh
-
-cat src/main/resources/test-fixtures/demo-workflow.json > $workflow_file
 
 die() {
 	echo $1 | boxes -d nuke
@@ -19,45 +12,65 @@ if [ -z "$SRV" ];then
 fi
 
 
-echoerr "# Replacing boilerplate URL with real URL"
-boilerplate_url="http://localhost:9998/api"
-if [ "$SRV" != "$boilerplate_url" ];then
-    echo "Replacing... $boilerplate_url with $SRV"
-    sed -i "s!http://localhost:9998/api!$SRV!" $workflow_file
-fi
+echoerr() {
+    echo "$@" 1>&2;
+}
 
 ##
 ## FIXME this will break if dm2e-ws repo is not in the parent directory
 ## TODO metadata
-echoerr "# publish some files"
-FILE1=$(zsh bin/ingest-file.sh ../dm2e-ws/src/test/resources/mappings/xslt/KBA_BBAW_TO_EDM.xsl)
-echo "FILE1='$FILE1'"
-FILE2=$(zsh bin/ingest-file.sh ../dm2e-ws/src/test/resources/provider-examples/dta/grimm_meistergesang_1811.TEI-P5.xml)
-echo "FILE2='$FILE2'"
+publishFiles() {
+    echoerr "# publish some files"
+    FILE1=$(zsh bin/ingest-file.sh ../dm2e-ws/src/test/resources/mappings/xslt/KBA_BBAW_TO_EDM.xsl)
+    echo "FILE1='$FILE1'"
+    FILE2=$(zsh bin/ingest-file.sh ../dm2e-ws/src/test/resources/provider-examples/dta/grimm_meistergesang_1811.TEI-P5.xml)
+    echo "FILE2='$FILE2'"
+}
 
-echoerr "# post workflow"
-WORKFLOW=$(POST -H $CT_JSON $SRV/workflow -d @$workflow_file \
-    2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
-echo "WORKFLOW='$WORKFLOW'"
-
-echoerr "# get empty config"
-GETJ $WORKFLOW/blankConfig 2>/dev/null | sed 's/bottles of beer on the wall/flailing piglets/' > $tempfile
-sed -i '/^{/a \
-"http://purl.org/dc/terms/creator":"http://omnom.hu-berlin.de/api/user/command-line",
-' $tempfile
+boilerplate_url="http://localhost:9998/api"
 
 
-echoerr "# persist it"
-CONFIG=$(POST -H $CT_JSON $SRV/config -d @$tempfile \
-    2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
-echo "CONFIG='$CONFIG'"
+publishWorkflow() {
+    wf=$1
 
-echoerr "# run a job"
-# echo POST -H "$CT_TEXT" "$WORKFLOW" -d "$CONFIG"
-# PUT "-H$CT_TEXT" "$WORKFLOW" -d "$CONFIG"
-JOB=$(PUT "-H$CT_TEXT" "$WORKFLOW" -d "$CONFIG" \
-    2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
-echo "JOB='$JOB'"
+    echoerr "----------------------"
+    echoerr "#"
+    echoerr "# WORKFLOW: $wf"
+    echoerr "#"
 
+    WORKFLOW=$(POST -H $CT_JSON $SRV/workflow -d @$wf \
+        2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
+    echo "WORKFLOW='$WORKFLOW'"
 
-echo $tempfile
+    echoerr "# get empty config"
+    GETJ $WORKFLOW/blankConfig 2>/dev/null | sed 's/bottles of beer on the wall/flailing piglets/' > $tempfile
+    sed -i '/^{/a \
+        "http://purl.org/dc/terms/creator":"http://omnom.hu-berlin.de/api/user/command-line",
+    ' $tempfile
+
+    echoerr "# persist it"
+    CONFIG=$(POST -H $CT_JSON $SRV/config -d @$tempfile \
+        2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
+    echo "CONFIG='$CONFIG'"
+
+    echoerr "# run a job"
+    JOB=$(PUT "-H$CT_TEXT" "$WORKFLOW" -d "$CONFIG" \
+        2>&1 | grep 'Location: ' | sed 's///' | grep 'Location' | grep -o 'http.*')
+    echo "JOB='$JOB'"
+
+    echoerr "# Replacing boilerplate URL with real URL"
+    if [ "$SRV" != "$boilerplate_url" ];then
+        echo "Replacing... $boilerplate_url with $SRV"
+        sed -i "s!http://localhost:9998/api!$SRV!" $wf_demo
+    fi
+}
+
+echoerr "# Create workflow tempfiles"
+tempfile=$(mktemp)
+wf_demo=$(mktemp)
+wf_xslt_publish=$(mktemp)
+cat src/main/resources/test-fixtures/demo-workflow.json > $wf_demo
+cat src/main/resources/test-fixtures/xslt-publish-workflow.json > $wf_xslt_publish
+
+publishWorkflow $wf_demo
+publishWorkflow $wf_xslt_publish
