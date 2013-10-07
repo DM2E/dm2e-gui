90b7160d0343732591fc067dd17d041afe7cc496
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-10-07 10:59:26 +0200

    Stable sort Web service list, #26

--- src/main/webapp/js/collections/workflow/WebserviceCollection.js
+++ src/main/webapp/js/collections/workflow/WebserviceCollection.js
@@ -1,41 +1,44 @@
 define([
-	'underscore',
-	'BaseCollection',
-	'logging',
-	'models/workflow/WebserviceModel',
-], function(_, BaseCollection, logging, WebserviceModel) {
+    'underscore',
+    'BaseCollection',
+    'logging',
+    'constants/RDFNS',
+    'models/workflow/WebserviceModel',
+], function(_, BaseCollection, logging, RDFNS, WebserviceModel) {
 
-	var log = logging.getLogger("WebserviceModel");
+    var log = logging.getLogger("WebserviceModel");
 
-	var WebserviceCollection = BaseCollection.extend({
+    var WebserviceCollection = BaseCollection.extend({
 
-		model : WebserviceModel,
-		
-//		initialize : function(models, options) {
-//			options = options || {};
-//			if (options.url) {
-//				this.url = function() {
-//					return options.url;
-//				};
-//			}
-//		},
-		
-//		 url : function() {
-//		 return 'api/file/list';
-//		 },
+        model : WebserviceModel,
 
-//		parse : function(data) {
-//			log.debug("Parsing list of files.");
-//			// console.log(data);
-//			return data;
-//		},
+        comparator : RDFNS.expand('rdfs:label'),
+        
+//      initialize : function(models, options) {
+//          options = options || {};
+//          if (options.url) {
+//              this.url = function() {
+//                  return options.url;
+//              };
+//          }
+//      },
+        
+//       url : function() {
+//       return 'api/file/list';
+//       },
 
-//		listAvailable : function() {
-//			return this.where({fileStatus : FILESTATUS.AVAILABLE});
-//		}
+//      parse : function(data) {
+//          log.debug("Parsing list of files.");
+//          // console.log(data);
+//          return data;
+//      },
 
-	});
+//      listAvailable : function() {
+//          return this.where({fileStatus : FILESTATUS.AVAILABLE});
+//      }
 
-	return WebserviceCollection;
+    });
+
+    return WebserviceCollection;
 
 });
