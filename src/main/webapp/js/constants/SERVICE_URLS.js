ca1a0711fa5dc1cfe39e18ec9e1400360ab61d84
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-11-21 11:34:24 +0100

    Properly handle port 80 (window.location.port is "" not 80 then)

--- src/main/webapp/js/constants/SERVICE_URLS.js
+++ src/main/webapp/js/constants/SERVICE_URLS.js
@@ -1,9 +1,9 @@
 define(function() {
     var protocol =  window.location.protocol;
-    var host = protocol +
-        "//" +
-        window.location.hostname +
-        (window.location.port !== 80 ? ":" + window.location.port : "");
+    var host = protocol + "//" + window.location.hostname;
+    if (window.location.port !== "") {
+        host += ":" + window.location.port;
+    }
     var path_without_file = window.location.pathname.replace(/[^\/]+$/, "");
     var baseURL = host + path_without_file + "api/";
     return {
